7347209fdc7f5ccd3aa4f8d71e761d8267ce9868
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-21 12:27:31 -0400

    revamp tests to fit new output style

--- R/outputs.R
+++ R/outputs.R
@@ -4,31 +4,30 @@
 #'
 #' @param x Object returned by \code{\link{fuzz_function}}.
 #' @param ... Additional arguments to be passed to or from methods.
-#' @param class_format How to format function return classes. \code{"concat"} (the
-#'   default) collapses multiple class names into one comma-delimited character
-#'   scalar, which can be easier to read on the console. \code{"nest"} creates a
-#'   nested data frame, saving object classes in a list column using
-#'   \code{\link[tidyr]{nest}}.
+#' @param delim The delimiter to use in fields like \code{messages} or
+#'    \code{warnings} where there may be multiple results.
+#' @param .id Quoted column name for the name of the fuzz input used for each
+#'    test.
 #'
 #' @return A data frame with the following columns: \describe{
 #'   \item{\code{fuzz_input}}{The name of the fuzz test performed.}
-#'   \item{\code{class}}{The class of response. If a condition was returned, one
-#'   of \code{message}, \code{warning}, or \code{error}; if a value was
-#'   returned, the \code{\link{class}} of that value.}
-#'   \item{\code{message}}{The
-#'   condition message, if applicable.}
+#'   \item{\code{messages}}{Delimited messages, if applicable.}
+#'   \item{\code{warnings}}{Delimited warnnings, if applicable.}
+#'   \item{\code{errors}}{Error returned, if applicable.}
+#'   \item{\code{value_classes}}{Delimited classes of the object returned by the
+#'    function, if applicable}
 #'   }
 #'
 #' @export
-as.data.frame.fuzz_results <- function(x, ..., sep = "; ", .id = "fuzz_input") {
-  purrr::map_df(x, parse_fuzz_result, sep, .id = .id)
+as.data.frame.fuzz_results <- function(x, ..., delim = "; ", .id = "fuzz_input") {
+  purrr::map_df(x, parse_fuzz_result_concat, delim, .id = .id)
 }
 
 #' Access the object returned by the fuzz test
 #'
 #' @param fr fuzz_results object
-#' @param index The test index (by position), or name, whose results to access. Same as the
-#'   row number in the data frame returned by
+#' @param index The test index (by position), or name, whose results to access.
+#'   Same as the row number in the data frame returned by
 #'   \code{\link{as.data.frame.fuzz_results}}.
 #'
 #' @export
@@ -43,8 +42,8 @@ compose_results <- function(fr) {
   structure(fr, class = "fuzz_results")
 }
 
-parse_fuzz_result <- function(fr, sep) {
-  fr$result_classes <- ifelse(is.null(fr$value), NA,
+parse_fuzz_result_concat <- function(fr, sep) {
+  fr$result_classes <- ifelse(is.null(fr$value), as.character(NA),
                               paste(class(fr$value), collapse = sep))
 
   fr <- purrr::map_at(fr, function(x) {
