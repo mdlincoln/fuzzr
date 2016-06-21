e6ad394286cd77b68b54095bd5bbe764138f1589
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-21 15:40:00 -0400

    package-wide documentation

---
+++ R/fuzzr-package.R
@@ -0,0 +1,8 @@
+#' Fuzz-Test R Functions
+#'
+#' Test function arugments with a wide array of vector inputs, and produce test
+#' reports summarizing messages, warnings, errors, and returned values.
+#'
+#' @name fuzzr
+#' @docType package
+NULL
